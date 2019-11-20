b412bca3e36090bd2c5b4ff0f4a0b1272e88c6e4
Brandon <itsbhenry@gmail.com>
2019-11-20 11:41:05 -0600

    changed port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/public/';
 var rpath = __dirname + '/public/';
+var PORT = process.env.PORT || 3001;
 
 router.use(function (req,res,next) {
     console.log("/" + req.method);
@@ -18,6 +19,6 @@ app.use(express.static(__dirname + '/resources'));
 
 app.use("/", router);
 
-app.listen(3001, function(){
+app.listen(PORT, function(){
     console.log("Live at Port 3001");
 });
