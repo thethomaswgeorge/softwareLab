896e80a49c19e6d9b4dffeeeb55626b86316efaf
Brandon Henry <itsbhenry@gmail.com>
2019-11-25 09:47:37 -0600

    fixed purchase function

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ let main = function(){
     });
 }();
 
-let url = "http://localhost:3001";
+let url = "http://localhost:3000";
 // let url = "";
 
 let $idSearchButton = $('#idSearch');
@@ -18,6 +18,7 @@ let $purchaseButton = $('.purchasebtn');
 //
 function searchById(){
  $idSearchButton.click(function(){
+
      console.log("search by id button clicked");
    $.ajax({
        url: url,
@@ -54,9 +55,12 @@ function searchByDate(){
 
 function purchase(){
     $purchaseButton.click(function(){
+
+        let price = $purchaseButton[0].getAttribute("price");
+        console.log(price);
         console.log("purchase button clicked");
         $.ajax({
-            url: url + "/purchase?price=3", // everything after ? is parameter that is being passed to route. the number should come from the
+            url: url + "/purchase?price=" + price, // everything after ? is parameter that is being passed to route. the number should come from the
             // item being purchased
             method: 'POST',
             crossDomain: true,
